4631ce47d2a1f7542a014123eba052afbd85b0b1
Christian Ernst <christian@ernst-menden.de>
2012-03-04 10:13:34 +0100

    scripts to change ip adress

---
+++ windows/ip-dhcp.bat
@@ -0,0 +1,2 @@
+netsh interface ipv4 set address name="LAN-Verbindung" source=dhcp
+netsh interface ipv4 set dnsservers name="LAN-Verbindung" source=dhcp
---
+++ windows/ip-static.bat
@@ -0,0 +1,2 @@
+netsh interface ipv4 set address "LAN-Verbindung" static X.X.X.X 255.255.255.0 X.X.X.X 1
+netsh interface ipv4 set dnsservers "LAN-Verbindung" static X.X.X.X primary
